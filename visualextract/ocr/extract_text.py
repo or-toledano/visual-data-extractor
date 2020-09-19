d03601293a29e47b21a493fd1c8ba067f6b7b1ff
Or Toledano <ortoledano@protonmail.com>
2020-09-19 18:12:39 +0300

    Remove comments

--- visualextract/ocr/extract_text.py
+++ visualextract/ocr/extract_text.py
@@ -16,9 +16,7 @@ def from_roi(roi):
     # blur = cv.bilateralFilter(roi, 0, 5, 5)
     # thresh = cv.adaptiveThreshold(roi, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C,
     #                               cv.THRESH_BINARY, 11, 2)
-    # wait_space(roi)
     thresh = cv.threshold(roi, 0, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)[1]
-    # wait_space(thresh)
     # OCR engine mode 2 is for Legacy + LSTM engines.
     # Page segmentation mode 6 is for a single uniform block of text
     text = pytesseract.image_to_string(thresh, config="-l eng --oem 3 --psm 6")
